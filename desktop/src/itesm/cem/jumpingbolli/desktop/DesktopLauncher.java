995aae6159d9f51ecb0d83ad71880b5fd8865b55
Fernando Melgar <fernandom.melgar@gmail.com>
2020-09-08 18:57:20 -0500

    Initial commit

---
+++ desktop/src/itesm/cem/jumpingbolli/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package itesm.cem.jumpingbolli.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import itesm.cem.jumpingbolli.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
